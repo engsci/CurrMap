41a63028d6e5523f9b2c854f87cb29458399e71b
James N. V. Cash <james.nvc@gmail.com>
2009-11-21 15:48:42 -0500

    Fixing Courses view

--- views.rb
+++ views.rb
@@ -1,19 +1,19 @@
 require 'json'
 require 'couch'
 
-db = "currmap"
-view = "testing"
+def add_views
+  db = "currmap"
+  view = "testing"
 
-views = {
-  "_id" => "_design/#{view}",
-  "views" => {
-    "Courses" => {
-      "map" => "function(doc) { if (doc[\"_id\"].match(/[A-Z]{3}[0-9]{3}/i) ) { emit(doc[\"_id\"], doc); } }"
+  views = {
+    "_id" => "_design/#{view}",
+    "views" => {
+      "Courses" => {
+        "map" => "function(doc) { if (doc[\"class\"] == \"Course\" ) { emit(doc[\"_id\"], doc); } }"
+      }
     }
   }
-}
 
-def add_views
   server = Couch::Server.new('localhost', 5984)
   server.put("/#{db}/_design/#{view}", views.to_json)
 end
