27789550074e1989652d88c25b72bab2680351c2
James N. V. Cash <james.nvc@gmail.com>
2009-11-25 10:18:08 -0500

    Factoring out initializer to common superclass

--- orm.rb
+++ orm.rb
@@ -3,6 +3,10 @@ require 'couch'
 
 class CouchDoc
   
+  def initialize(id)
+    @couch_data = get_by_id id
+  end
+  
   def get_by_id(id)
     return JSON.parse(Couch::Server.new('localhost', 5984).get("/currmap/#{id}").body)
   end
@@ -11,7 +15,7 @@ class CouchDoc
 
     def field_getters(names)
       names.each do |name|
-        define_method name do
+        define_method name.gsub(/ /, '_') do
           return @couch_data[name]
         end
       end
@@ -23,25 +27,16 @@ end
 
 class Course < CouchDoc
   attr_reader :staff
-  field_getters %w(name activities weight)
+  field_getters %w(name activities weight calendar\ entry)
   
   def initialize(course_code)
-    @couch_data = get_by_id course_code
+    super course_code
     @staff = @couch_data["staff"].keys.map { |prof| Staff.new prof }
-  end
-  
-  def calendar_entry
-    @couch_data["calendar\ entry"]
   end
   
 end
 
 class Staff < CouchDoc
   field_getters %w(email name phone website)
-  
-  def initialize(name)
-    @couch_data = get_by_id name
-  end
-  
 end
 
