e80563381c94a00403c66f540de6d70a7fdfb602
James N. V. Cash <james.nvc@gmail.com>
2009-11-25 09:44:07 -0500

    Fixes to ORM layer

--- orm.rb
+++ orm.rb
@@ -1,31 +1,47 @@
 require 'json'
 require 'couch'
 
-module CurrMap
+class CouchDoc
   
-  class << self
-    attr_reader :server
-    @@server = Couch::Server.new('localhost', 5984)
-    def get_by_id(id)
-      return JSON.parse(@@server.get("/currmap/#{id}").body)
-    end
+  def get_by_id(id)
+    return JSON.parse(Couch::Server.new('localhost', 5984).get("/currmap/#{id}").body)
   end
   
-  class Course
+  class << self
 
-    attr_reader :staff
-    
-    def initialize(course_code)
-      @course = CurrMap.get_by_id course_code
-      @staff = @course["staff"].keys.map { |prof| CurrMap.get_by_id prof }
-    end
-    
-    %w(name activities calendar\ entry weight class).each do |field|
-      define_method field do
-        return @course[field]
+    def field_getters(names)
+      names.each do |name|
+        define_method name do
+          return @couch_data[name]
+        end
       end
     end
     
   end
   
 end
+
+class Course < CouchDoc
+  attr_reader :staff
+  field_getters %w(name activities weight)
+  
+  def initialize(course_code)
+    @couch_data = get_by_id course_code
+    @staff = @couch_data["staff"].keys.map { |prof| Staff.new prof }
+  end
+  
+  def calendar_entry
+    @couch_data["calendar\ entry"]
+  end
+  
+end
+
+class Staff < CouchDoc
+  field_getters %w(email name phone website)
+  
+  def initialize(name)
+    @couch_data = get_by_id name
+  end
+  
+end
+
