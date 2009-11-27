9b2796a222ff8a0344675671d4390f60eeb47c58
James N. V. Cash <james.nvc@gmail.com>
2009-11-27 18:15:20 -0500

    Not using views.js anymore

--- public/views.js
+++
@@ -1,8 +0,0 @@
-var views = {
-  "_id": "_design/testing",
-  "views": {
-    "Courses": {
-      "map": "function(doc) { if (doc[\"_id\"].match(/[A-Z]{3}[0-9]{3}/i) ) { emit(doc[\"_id\"], doc); } }"
-    }
-  }
-};
