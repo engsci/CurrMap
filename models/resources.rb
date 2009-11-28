535b18e39af627d712c39576aaf3e8caa6c47918
James N. V. Cash <james.nvc@gmail.com>
2009-11-27 19:54:36 -0500

    Switching models to not use field_accessors anymore

--- models/resources.rb
+++ models/resources.rb
@@ -1,5 +1,4 @@
 require 'orm'
 
 class Resource < CouchDoc
-  field_getters %w(ISBN edition name author publisher type)
 end
--- models/staff.rb
+++ models/staff.rb
@@ -1,6 +1,5 @@
 require 'orm'
 
 class Staff < CouchDoc
-  field_getters %w(email name phone website)
   id_accessor :short_name
 end
