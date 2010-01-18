31e427db09b4ca1a17d9eb60569788b44089f6c6
James N. V. Cash <james.nvc@gmail.com>
2010-01-18 09:44:33 -0500

    Commenting changes to staff.rb

--- models/staff.rb
+++ models/staff.rb
@@ -8,14 +8,15 @@ class Staff < CouchDoc
     if sname.kind_of? Hash
       @couch_data = sname
     else
+      # Using the CoursesByProf view to get both the professor and the courses they teach
       uri = File.join("/", @@db, "_design" , @@design_doc, "_view",
                       "CoursesByProf?startkey=[\"#{sname}\"]&endkey=[\"#{sname}\",2]")
       couch_got = JSON.parse(Couch::Server.new(@@couch_uri, @@couch_port).get(uri).body)["rows"]
+      @couch_data = couch_got[0]["value"]
       courses = couch_got[1..-1]
       if not courses.nil?
         @courses_taught = courses.map { |c| c["value"] }
       end
-      @couch_data = couch_got[0]["value"]
     end
   end
 end
