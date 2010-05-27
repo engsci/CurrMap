e81998040e27075d647f1308b5c7d5f506cb819b
James N. V. Cash <james.nvc@gmail.com>
2010-05-26 23:24:20 -0400

    Refactoring and fixing ToK code

--- tok.rb
+++ tok.rb
@@ -9,24 +9,53 @@ module TreeOfKnowledge
       @user = username
       @pass = password
     end
+    
+    def get_uri(path)
+      URI.parse("http://#{@host}:#{@port}#{path}")
+    end
 
     def get_resource(path)
-      uri = URI.parse("http://" + host + ":" + port + path)
+      uri = get_uri(path)
       req = Net::HTTP::Get.new(uri.path)
-      res = Net::HTTP.start(uri.host, uri.port) do |http|
-        http.request(req)
-      end
+      res = request(req)
       Marshal.load(res.body)
     end
 
     def add_resource(path)
-      uri = URI.parse("http://" + host + ":" + port + path)
+      uri = get_uri(path)
       req = Net::HTTP::Put.new(uri.path)
       req.basic_auth @user, @pass
-      res = Net::HTTP.start(uri.host, uri.port) do |http|
-        http.request(req)
-      end
+      res = request(req)
       Marshal.load(res.body)
     end
+    
+    def request(req)
+      begin
+        res = Net::HTTP.start(@host, @port) do |http|
+          http.request(req)
+        end
+      rescue Errno::ECONNREFUSED
+        handle_error(req, res)
+      end
+      if not res.kind_of? Net::HTTPSuccess
+        handle_error(req, res)
+      end
+      return res
+    end
+    
+    def handle_error(req, res)
+      e = ConnectionError.new(req, res)
+      raise e
+    end
+    
   end
+  
+  class ConnectionError < RuntimeError
+    attr_reader :result, :request
+    def initialize(request, result)
+      @request = request
+      @result = result
+    end
+  end
+    
 end
