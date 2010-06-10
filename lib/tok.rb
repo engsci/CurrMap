651313b3f59792dde47d31729da7c8e3f0fe4cf8
James N. V. Cash <james.nvc@gmail.com>
2010-06-10 12:37:20 -0400

    Adding ability to add synonyms and arbitrary relationships via the REST ToK interface

--- lib/tok.rb
+++ lib/tok.rb
@@ -28,8 +28,24 @@ module TreeOfKnowledge
     end
 
     def add_resource(path)
+      put_to path
+    end
+    
+    def add_synonym(word1, word2, preferred=false)
+      put_to "Synonym/#{word1}/#{word2}#{if preferred then '?preferred=true' else '' end}"
+    end
+    
+    def add_relation(item1, reln, item2)
+      put_to "Relation/#{item1}/#{reln}/#{item2}"
+    end
+    
+    def put_to(path)
       uri = get_uri(path)
-      req = Net::HTTP::Put.new(uri.path)
+      full_path = uri.path
+      if !uri.query.nil?
+        full_path << "?" << uri.query
+      end
+      req = Net::HTTP::Put.new(full_path)
       req.basic_auth @user, @pass
       res = request(req)
       Marshal.load(res.body)
