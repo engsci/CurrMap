efb7f90ef543c7d5df9637d86c211632da791bf6
Rafal Dittwald <rafal.dittwald@gmail.com>
2010-07-13 18:23:17 -0400

    searchbox auto-text

---
+++ public/javascripts/search.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  //$('#ferret_search').focus();
+  default_msg = $('#ferret_search').val();
+  $('#ferret_search').focus(
+    function() {
+      if ($(this).attr('value') == default_msg) $(this).attr('value', '');
+      $(this).addClass("active");
+    }
+  ).blur(
+    function() {
+      if ($(this).attr('value') == '') $(this).attr('value', default_msg);
+      $(this).removeClass("active");
+    }
+  );
+});
