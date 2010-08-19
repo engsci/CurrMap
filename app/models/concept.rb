2ff50dd421c4339226a2cbcf36dcc91a2d5ed7d4
Rafal Dittwald <rafal.dittwald@gmail.com>
2010-08-19 16:56:28 -0400

    remove embedded_in from concep b/c REE is pissy

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,4 +1,4 @@
 class Concept
   #references_many :courses, :stored_as => :array, :inverse_of => :related_concepts
-  embedded_in :courses, :inverse_of => :concepts
+  #embedded_in :courses, :inverse_of => :concepts
 end
