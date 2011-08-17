307246c2c8f2300294a466d0bcf61db93a846be9
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-08-17 13:45:44 +0200

    fix tabs/spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,21 @@ class ApplicationController < ActionController::Base
   
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
+    
     if user_signed_in?
-        redirect_to unauthorized_url
+      redirect_to unauthorized_url
     else
-        redirect_to new_user_session_url (:next => request.path)
+      redirect_to new_user_session_url(:next => request.path)
     end
+    
   end
+  
   def url_escape(string)
     string.gsub(/([^ a-zA-Z0-9_.-]+)/n) do
     '%' + $1.unpack('H2' * $1.size).join('%').upcase
     end.tr(' ', '+')
   end
   
-
-
   protect_from_forgery
   layout 'application'
   
